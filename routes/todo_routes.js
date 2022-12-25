9efed9e4f16c2d0447093be5ed3a26d127cfde07
Maxim Shcherbakov <cherbakov.2010@mail.ru>
2022-12-25 21:05:53 +0300

    second try to rid of cors

--- routes/todo_routes.js
+++ routes/todo_routes.js
@@ -5,11 +5,7 @@ const cors = require('cors')
 const router = Router()
 
 
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-    corsOptions = { origin: false }
-  callback(null, corsOptions)
-}
+var corsOptionsDelegate = {origin: "*"};
 
 router.get('/get_all_todos', cors(corsOptionsDelegate), controllers.GetRetrieveAll);
 router.get('/get_todo', cors(corsOptionsDelegate), controllers.GetRetrieveById);
