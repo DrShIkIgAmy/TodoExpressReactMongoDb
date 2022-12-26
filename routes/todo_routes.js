b7225d970776fe4de8213ad5c94aef08ed3922e9
Maxim Shcherbakov <cherbakov.2010@mail.ru>
2022-12-26 20:03:06 +0300

    Log added

--- routes/todo_routes.js
+++ routes/todo_routes.js
@@ -10,10 +10,16 @@ var corsOptionsDelegate = {
   methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
 };
 
+const log_func = (req, res, next)=> {
+  console.log('Received request');
+  console.log(req);
+  next();
+}
+
 router.options('*', cors(corsOptionsDelegate));
 router.post('/add_todo', cors(corsOptionsDelegate), controllers.PostAdd);
 router.get('/get_all_todos', cors(corsOptionsDelegate), controllers.GetRetrieveAll);
-router.get('/get_todo', cors(corsOptionsDelegate), controllers.GetRetrieveById);
+router.get('/get_todo', cors(corsOptionsDelegate), log_func, controllers.GetRetrieveById);
 
 router.delete('/delete_all', cors(corsOptionsDelegate), controllers.PostRemoveAll);
 router.delete('/delete_todo', cors(corsOptionsDelegate), controllers.PostRemoveById);
